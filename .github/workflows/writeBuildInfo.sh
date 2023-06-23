a570782920ffa07f0f29785c868f965ad782a29f
Martijn Dirkse <martijn@integrationpartners.nl>
2023-06-23 10:14:55 +0200

    feat: update CI/CD

--- .github/workflows/writeBuildInfo.sh
+++ .github/workflows/writeBuildInfo.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-echo "version=$1" > src/main/configurations/LopendeZaken/BuildInfo.properties
+if [[ -n $1 ]]; then
+  echo "writeBuildInfo.sh - writing version $1"
+  echo "version=$1" > src/main/configurations/LopendeZaken/BuildInfo.properties
+  echo "versionDate_ddmmyyyy=$(date +%d/%m/%Y)" >> src/main/configurations/LopendeZaken/BuildInfo.properties
+else
+  echo "writeBuildInfo.sh - no version to write, leaving BuildInfo.properties unchanged"
+fi
