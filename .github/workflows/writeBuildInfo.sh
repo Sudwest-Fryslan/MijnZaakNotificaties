8a6e91820588c5ea9ecec6258509d321cc2e899b
Martijn Dirkse <martijn@integrationpartners.nl>
2023-06-23 11:42:48 +0200

    fix: Show the version in the console

--- .github/workflows/writeBuildInfo.sh
+++ .github/workflows/writeBuildInfo.sh
@@ -2,8 +2,7 @@
 
 if [[ -n $1 ]]; then
   echo "writeBuildInfo.sh - writing version $1"
-  echo "version=$1" > src/main/configurations/LopendeZaken/BuildInfo.properties
-  echo "versionDate_ddmmyyyy=$(date +%d/%m/%Y)" >> src/main/configurations/LopendeZaken/BuildInfo.properties
+  echo "instance.version=$1" > src/main/resources/BuildInfo.properties
 else
   echo "writeBuildInfo.sh - no version to write, leaving BuildInfo.properties unchanged"
 fi
